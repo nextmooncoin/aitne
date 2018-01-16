4303c20baa8413278461c26741633dc9b24827d0
nextmooncoin <nextmooncoin@amagaki.lan>
2018-01-15 18:10:55 -0800

    add empty project and empty kit

---
+++ AitneKit/AitneKit.h
@@ -0,0 +1,13 @@
+//  Copyright © 2018 nextmooncoin. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AitneKit.
+FOUNDATION_EXPORT double AitneKitVersionNumber;
+
+//! Project version string for AitneKit.
+FOUNDATION_EXPORT const unsigned char AitneKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AitneKit/PublicHeader.h>
+
+
